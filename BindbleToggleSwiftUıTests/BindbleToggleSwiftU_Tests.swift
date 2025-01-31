f8d52e40fa00677ecd0a3821c5a608ed6f8034fc
Ömer Apaydın <omerapaydin90@gmail.com>
2025-01-31 18:25:18 +0300

    Initial Commit

---
+++ BindbleToggleSwiftUıTests/BindbleToggleSwiftU_Tests.swift
@@ -0,0 +1,17 @@
+//
+//  BindbleToggleSwiftU_Tests.swift
+//  BindbleToggleSwiftUıTests
+//
+//  Created by Ömer on 31.01.2025.
+//
+
+import Testing
+@testable import BindbleToggleSwiftU_
+
+struct BindbleToggleSwiftU_Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
