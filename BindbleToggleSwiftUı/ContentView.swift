92e69505e758dcf211f5ad38f1352796f2057b92
Ömer Apaydın <omerapaydin90@gmail.com>
2025-01-31 18:42:01 +0300

    First

--- BindbleToggleSwiftUı/ContentView.swift
+++ BindbleToggleSwiftUı/ContentView.swift
@@ -8,54 +8,40 @@
 import SwiftUI
 import SwiftData
 
-struct ContentView: View {
-    @Environment(\.modelContext) private var modelContext
-    @Query private var items: [Item]
+@Observable
+class DarkTheme  {
+    var isEnable : Bool = false
+}
 
+
+struct DarkChoiceView : View {
+    
+    @Bindable var darkTheme : DarkTheme
+    
     var body: some View {
-        NavigationSplitView {
-            List {
-                ForEach(items) { item in
-                    NavigationLink {
-                        Text("Item at \(item.timestamp, format: Date.FormatStyle(date: .numeric, time: .standard))")
-                    } label: {
-                        Text(item.timestamp, format: Date.FormatStyle(date: .numeric, time: .standard))
-                    }
-                }
-                .onDelete(perform: deleteItems)
-            }
-            .toolbar {
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    EditButton()
-                }
-                ToolbarItem {
-                    Button(action: addItem) {
-                        Label("Add Item", systemImage: "plus")
-                    }
-                }
-            }
-        } detail: {
-            Text("Select an item")
+        Toggle(isOn: $darkTheme.isEnable){
+            EmptyView()
+        }.fixedSize()
+    }
+    
+}
+
+struct ContentView: View {
+
+    @State private var darkTheme = DarkTheme()
+    
+    var body: some View {
+        VStack{
+            DarkChoiceView(darkTheme: darkTheme)
+                
         }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(darkTheme.isEnable ? .black : .white)
     }
 
-    private func addItem() {
-        withAnimation {
-            let newItem = Item(timestamp: Date())
-            modelContext.insert(newItem)
-        }
-    }
-
-    private func deleteItems(offsets: IndexSet) {
-        withAnimation {
-            for index in offsets {
-                modelContext.delete(items[index])
-            }
-        }
-    }
+ 
 }
 
 #Preview {
     ContentView()
-        .modelContainer(for: Item.self, inMemory: true)
 }
